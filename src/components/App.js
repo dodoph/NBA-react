4090967df8ac83040e86c819f77448e8c4f1f43d
Jessie Tang <tangqiaojie@hotmail.com>
2020-02-10 10:48:43 -0800

    add App file

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/App.css';
+import TopNavBar from "./TopNavBar"
+
+function App() {
+  return (
+    <div className="App">
+      <TopNavBar>
+
+      </TopNavBar>
+    </div>
+  );
+}
+
+export default App;
