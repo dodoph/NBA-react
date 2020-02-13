f5638c03bbcf3d08db27a62e79140c9e999e6c84
Jessie Tang <tangqiaojie@hotmail.com>
2020-02-12 19:48:01 -0800

    add data view container to main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
-
 import nba from '../nba-client';
 import Profile from './Profile';
-import ShotChart from './ShotChart';
+import DataViewContainer from "./DataViewContainer";
 
 class Main extends Component {
     state = {
@@ -11,21 +10,25 @@ class Main extends Component {
     }
 
     componentDidMount() {
-        window.nba = nba;
+        window.nba = nba; //define global variable
+        //playerId is getting from front end
+        //nba.findPlayer('Stephen Curry').playerId 解构用法
+        //PlayerID: xxx 赋值
         nba.stats.playerInfo({ PlayerID: nba.findPlayer('Stephen Curry').playerId})
+            //call back: 'then' is called until all the data is fetched successfully, and store into 'info'
             .then((info) => {
                 console.log(info);
-                const playInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+                const playInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);//合并数据
                 console.log(playInfo);
-                this.setState({ playerInfo: playInfo });
+                this.setState({ playerInfo: playInfo }); //set states
             })
     }
-
+    //通过props 将state 传给了 Profile
     render() {
         return (
             <div className="main">
                 <Profile playerInfo={this.state.playerInfo} />
-                <ShotChart playerId={this.state.playerId}/>
+                <DataViewContainer playerId={this.state.playerId}/>
             </div>
         );
     }
