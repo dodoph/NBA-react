d5c5e066a51a5f67565a9cf2a202e861ca1156cd
Jessie Tang <tangqiaojie@hotmail.com>
2020-02-23 11:34:37 -0800

    updata app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import '../styles/App.css';
-import TopNavBar from "./TopNavBar"
+
+import TopNavBar from './TopNavBar';
+import Main from './Main';
 
 function App() {
-  return (
-    <div className="App">
-      <TopNavBar>
-
-      </TopNavBar>
-    </div>
-  );
+    return (
+        <div className="App">
+            <TopNavBar />
+            <Main />
+        </div>
+    );
 }
 
 export default App;
