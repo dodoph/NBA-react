4a1a0c784a5091f782f61f84524d84e8865dbbf3
Jessie Tang <tangqiaojie@hotmail.com>
2020-02-23 11:33:42 -0800

    updata main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,34 +1,49 @@
 import React, {Component} from 'react';
 import nba from '../nba-client';
 import Profile from './Profile';
-import DataViewContainer from "./DataViewContainer";
+import DataViewContainer from './DataViewContainer';
+import SearchBar from './SearchBar';
+
+import { DEFAULT_PLAYER_INFO } from '../constants';
+
 
 class Main extends Component {
     state = {
-        playerInfo: {},
-        playerId: 201939,
+        playerInfo: DEFAULT_PLAYER_INFO
     }
 
     componentDidMount() {
-        window.nba = nba; //define global variable
+        window.nba = nba;
+        this.loadPlayerInfo('Stephen Curry');
+    }
+    loadPlayerInfo = (playerName) => {
         //playerId is getting from front end
         //nba.findPlayer('Stephen Curry').playerId 解构用法
         //PlayerID: xxx 赋值
-        nba.stats.playerInfo({ PlayerID: nba.findPlayer('Stephen Curry').playerId})
-            //call back: 'then' is called until all the data is fetched successfully, and store into 'info'
-            .then((info) => {
-                console.log(info);
-                const playInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);//合并数据
-                console.log(playInfo);
-                this.setState({ playerInfo: playInfo }); //set states
-            })
+        nba.stats.playerInfo({ PlayerID: nba.findPlayer(playerName).playerId}).then((info) => { //call back: 'then' is called until all the data is fetched successfully, and store into 'info'
+            console.log(info);
+            const playInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+            console.log(playInfo);
+            this.setState({ playerInfo: playInfo });//set states
+        })
     }
+
+
+    handleSelectPlayer = (playerName) => {
+        this.loadPlayerInfo(playerName);
+    }
+
+
+
     //通过props 将state 传给了 Profile
     render() {
         return (
             <div className="main">
-                <Profile playerInfo={this.state.playerInfo} />
-                <DataViewContainer playerId={this.state.playerId}/>
+                <SearchBar handleSelectPlayer={this.handleSelectPlayer}/>
+                <div className="player">
+                    <Profile playerInfo={this.state.playerInfo} />
+                    <DataViewContainer playerId={this.state.playerInfo.playerId}/>
+                </div>
             </div>
         );
     }
